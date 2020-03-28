b1d9e5b2af33a0e04f5bf28ee30cacdcc659f805
lesley <lesleybanadzem2017@gmail.com>
2020-03-28 11:50:29 -0700

    Added some styles and content to the bottom half of the header

--- src/components/Aside/Header/Header.jsx
+++ src/components/Aside/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Search, Justify} from 'react-bootstrap-icons';
+import {Search, Justify, PersonBoundingBox, Bell } from 'react-bootstrap-icons';
 import './Header.css'
 
 function Header() {
@@ -7,18 +7,20 @@ function Header() {
         <div className='header'>
             <div className='top'>
                 <div className='burger-menu'>
-                    <Justify  size={45} />
+                    <Justify  size={35} />
                 </div>
                 <div className='input-field'>
                     <input type='text' className="rq-form-element" name='search'/>
                     <i></i>
                     <div>
-                        <Search size={30}/>
+                        <Search size={25}/>
                     </div>
                 </div>
             </div>
             <div className='bottom'>
-
+                <div className="name-icon"> <PersonBoundingBox size={30}/></div>
+                <div className="name-tag"> Lesley Banadzem</div>
+                <div className="notifications"> <Bell size={30} /></div>
             </div>
         </div>
     )
