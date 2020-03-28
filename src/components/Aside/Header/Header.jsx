338ab9b3a262380f40f6b543ec1d3b3e7afc61b0
lesley <lesleybanadzem2017@gmail.com>
2020-03-28 11:36:31 -0700

    Added styles to the top section of the header

--- src/components/Aside/Header/Header.jsx
+++ src/components/Aside/Header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {Search, Justify} from 'react-bootstrap-icons';
+import './Header.css'
 
 function Header() {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className='header'>
+            <div className='top'>
+                <div className='burger-menu'>
+                    <Justify  size={45} />
+                </div>
+                <div className='input-field'>
+                    <input type='text' className="rq-form-element" name='search'/>
+                    <i></i>
+                    <div>
+                        <Search size={30}/>
+                    </div>
+                </div>
+            </div>
+            <div className='bottom'>
+
+            </div>
         </div>
     )
 }
