10f5745c231e55983e58c8b1b1735eb682f3a294
lesley <lesleybanadzem2017@gmail.com>
2020-03-28 17:47:33 -0700

    Completed card to add todo

--- src/components/Main/TodoList/TodoCard.jsx
+++ src/components/Main/TodoList/TodoCard.jsx
@@ -1,24 +1,26 @@
-import React, {useState} from 'react';
-import { Star, StarFill } from "react-bootstrap-icons";
-import { connect } from 'react-redux';
-
+import React, { useState } from "react"
+import { Star, StarFill } from "react-bootstrap-icons"
+import { connect } from "react-redux"
 
 function TodoCard(props) {
-    const [favorite, setfavorite] = useState(false)
+  const [favorite, setfavorite] = useState(false)
 
-    const displayFav = () => {
-        return favorite ? <StarFill /> : <Star />
-      }
-    
-    return (
-        <div className='todo-card'>
-            <div>
-                <input type='checkbox' className = 'todo-completed' />
-                <h5 className='todo-title'>{props.name}</h5>
-            </div>
-            {displayFav()}
-        </div>
-    )
+  const displayFav = () => {
+    return favorite ? <StarFill /> : <Star />
+  }
+
+  return (
+    <div className="todo-card">
+      <div>
+        <input type="checkbox" className="todo-completed" />
+        <h5 className="todo-title">{props.todo.name}</h5>
+      </div>
+      <div onClick={e => {
+          e.preventDefault()
+          setfavorite(!favorite)}
+          }>{displayFav()}</div>
+    </div>
+  )
 }
 
 export default connect(null, {})(TodoCard)
--- src/components/Main/TodoList/TodoList.jsx
+++ src/components/Main/TodoList/TodoList.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import TodoCard from './TodoCard'
 import './TodoList.css'
+import { connect } from 'react-redux'
 
-function TodoList() {
+function TodoList(props) {
+    const [todo, setTodo] = useState([]);
+
+    useEffect(()=>{ 
+        setTodo(props.todo)
+    },[props.todo])
+
+    const displayCards = () => {
+        if(todo.length <= 0 ){
+            return null
+        }else{
+           return todo.map((item, index) => <TodoCard key={index} todo={item}/>)
+        }
+    }
     return (
         <div>
-            <TodoCard />
+            {displayCards()}
         </div>
     )
 }
 
-export default TodoList
+const mapstateToProps = state => {
+    console.log(state)
+    return {
+        todo:  state.todoreducer.todo
+    }
+}
+export default connect(mapstateToProps, {})(TodoList)
